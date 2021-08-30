ad2ca13eb9bf0ea51935cf8e07565cad2916b282
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2021-08-30 15:34:38 +0530

    done

---
+++ wheather-app/src/component/wheather/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./style.css";
+
+const Temp = () =>{
+    return (
+        <>
+        <div className="wrap">
+            <div className="search">
+                <input 
+                    type="search"
+                    placeholder="search...."
+                    autoFocus
+                    id="search"
+                    className="searchTerm"
+                />
+
+                <button className="searchButton" type="button">
+                search
+                </button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Temp;
